ba989eb6d8e58db459fdee482083e4bf30158c64
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2023-08-17 13:24:42 -0700

    accept custom vm name

--- setup.sh
+++ setup.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 set -eu
 
+NAME=${NAME:-nixos}
+ORB_CMD="orb -m $NAME"
+
 # create vm
-orbctl create nixos
-orbctl default nixos
+orbctl create nixos "$NAME"
+orbctl default "$NAME"
 
 # install home-manager
-orb nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz home-manager
-orb nix-channel --update
-orb nix-shell '<home-manager>' -A install
+$ORB_CMD nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz home-manager
+$ORB_CMD nix-channel --update
+$ORB_CMD nix-shell '<home-manager>' -A install
 
 # run home-manager
 DOTFILES_DIR=/Users/nate/code/dotfiles
-orb home-manager switch -f "${DOTFILES_DIR}/home-manager/home.nix"
+$ORB_CMD home-manager switch -f "${DOTFILES_DIR}/home-manager/home.nix"
 
 # run nixos-rebuild
-orb sudo nixos-rebuild switch -I nixos-config="${DOTFILES_DIR}/configuration.nix"
+$ORB_CMD sudo nixos-rebuild switch -I nixos-config="${DOTFILES_DIR}/configuration.nix"

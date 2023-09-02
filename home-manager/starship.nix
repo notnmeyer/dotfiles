82532a917b044332a9521edcfcb4b271b1d7a8f7
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2023-09-01 21:46:23 -0700

    tweak prompt
    tweak prompt

--- home-manager/starship.nix
+++ home-manager/starship.nix
@@ -6,18 +6,9 @@
 
     # Configuration written to ~/.config/starship.toml
     settings = {
-      # add_newline = false;
-
-      # character = {
-      #   success_symbol = "[➜](bold green)";
-      #   error_symbol = "[➜](bold red)";
-      # };
-
-      # package.disabled = true;
-      aws = {
-        # format = "on [$symbol($profile )(\($region\) )]($style)";
-        # style = "bold blue";
-        # symbol = "🅰 ";
+      character = {
+        success_symbol = "[➜](bold green)";
+        error_symbol = "[➜](bold red)";
       };
 
       gcloud = {

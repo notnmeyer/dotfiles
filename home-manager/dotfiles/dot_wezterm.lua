9a5c048f7b25240e97afb100fd8a16b003c2480f
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2023-09-04 21:07:37 -0700

    update wezterm config
    update wezterm config

--- home-manager/dotfiles/dot_wezterm.lua
+++ home-manager/dotfiles/dot_wezterm.lua
@@ -5,17 +5,29 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.color_scheme = 'Catppuccin Mocha'
-config.font = wezterm.font 'Anonymous Pro'
-config.font_size = 16
-config.adjust_window_size_when_changing_font_size = false
-config.enable_tab_bar = false
+-- colors
+config.color_scheme = 'Ayu Mirage'
+config.colors = {
+  cursor_fg = 'white'
+}
+
+-- window
+config.window_decorations = "RESIZE"
 config.window_padding = {
   left = '1cell',
   right = '1cell',
-  top = '0cell',
-  bottom = '0cell',
+  top = 0,
+  bottom = 0,
 }
 
-return config
+-- fonts
+config.font = wezterm.font 'Anonymous Pro'
+config.font_size = 16
+config.adjust_window_size_when_changing_font_size = false
 
+-- tabs
+config.enable_tab_bar = true
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+
+return config

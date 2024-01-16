373608b9344d0a4af9c5e5db971a05d6c9bd68b5
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2024-01-15 20:19:52 -0800

    enable flakes
    enable flakes

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -5,6 +5,11 @@
 { config, pkgs, ... }:
 
 {
+  nix = {
+    package = pkgs.nix;
+    settings.experimental-features = [ "nix-command" "flakes" ];
+  };
+
   imports = [
     ./fish.nix
     ./git.nix

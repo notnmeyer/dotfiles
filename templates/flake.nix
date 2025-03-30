9ffe691519cf6e0ba2ee2d1b08808011254714a0
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2025-03-29 21:01:55 -0700

    stub a new project

---
+++ templates/flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+  outputs = {
+    systems,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    eachSystem = f:
+      nixpkgs.lib.genAttrs (import systems) (
+        system:
+          f nixpkgs.legacyPackages.${system}
+      );
+  in {
+    devShells = eachSystem (pkgs: {
+      default = pkgs.mkShell {
+        buildInputs = [
+          # set your deps
+          pkgs.nodejs_22
+        ];
+      };
+    });
+  };
+}

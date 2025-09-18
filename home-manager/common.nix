46e6bad84abcab32b227bc1e9d37e7833a93e3f2
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2025-09-17 20:39:10 -0700

    add ncurses, zlib

--- home-manager/common.nix
+++ home-manager/common.nix
@@ -28,6 +28,7 @@
   lua-language-server
   mosh
   # libgcc # not available on macos :\
+  ncurses
   nil # nix ls
   nixfmt
   nmap
@@ -44,5 +45,6 @@
   watchexec
   wget
   yq
+  zlib
   zoxide
 ]

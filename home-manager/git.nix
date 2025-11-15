7e7dcb9fe6ea9c8f2881cb167054130f000d31ad
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2025-11-14 16:05:55 -0800

    fix some things that were moved around

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -1,11 +1,58 @@
 {
   programs.git = {
     enable = true;
-    userName = "Nate Meyer";
-    userEmail = "672246+notnmeyer@users.noreply.github.com";
-    aliases = {
-      co = "checkout";
-      cob = "checkout -b";
+    settings = {
+      user = {
+        name = "Nate Meyer";
+        email = "672246+notnmeyer@users.noreply.github.com";
+      };
+
+      aliases = {
+        co = "checkout";
+        cob = "checkout -b";
+      };
+
+      extraConfig = {
+        apply = {
+          whitespace = "fix";
+        };
+        branch = {
+          sort = "-committerdate";
+        };
+        column = {
+          ui = "auto";
+        };
+        core = {
+          whitespace = "trailing-space,space-before-tab";
+        };
+        diff = {
+          algorithm = "histogram";
+          colorMoved = "plain";
+          mnemonicPrefix = "true";
+          renames = "true";
+        };
+        fetch = {
+          prune = "true";
+          pruneTags = "true";
+          all = "true";
+        };
+        init = {
+          defaultBranch = "main";
+        };
+        push = {
+          autoSetupRemote = "true";
+          default = "simple";
+          followTags = "true";
+        };
+        rebase = {
+          autoSquash = "true";
+          autoStash = "true";
+          updateRefs = "true";
+        };
+        tag = {
+          sort = "version:refname";
+        };
+      };
     };
     includes = [
       {
@@ -13,47 +60,6 @@
         path = "~/code/work/.gitconfig-work";
       }
     ];
-    extraConfig = {
-      apply = {
-        whitespace = "fix";
-      };
-      branch = {
-        sort = "-committerdate";
-      };
-      column = {
-        ui = "auto";
-      };
-      core = {
-        whitespace = "trailing-space,space-before-tab";
-      };
-      diff = {
-        algorithm = "histogram";
-        colorMoved = "plain";
-        mnemonicPrefix = "true";
-        renames = "true";
-      };
-      fetch = {
-        prune = "true";
-        pruneTags = "true";
-        all = "true";
-      };
-      init = {
-        defaultBranch = "main";
-      };
-      push = {
-        autoSetupRemote = "true";
-        default = "simple";
-        followTags = "true";
-      };
-      rebase = {
-        autoSquash = "true";
-        autoStash = "true";
-        updateRefs = "true";
-      };
-      tag = {
-        sort = "version:refname";
-      };
-    };
   };
 
   programs.gh = {

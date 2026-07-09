d3dcb6369452329bdd53a191b45728a40813eae7
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2026-07-08 22:45:56 -0700

    j function for interactive jobs picker

---
+++ dotfiles/.config/fish/functions/j.fish
@@ -0,0 +1,16 @@
+function j --description 'pick a job with fzf; enter=fg, ctrl+b=bg'
+    set -l sel (jobs | fzf --expect=enter,ctrl-b \
+        --header="pick a job to resume\nenter: fg · ctrl-b: bg" --header-first)
+    test (count $sel) -ge 2; or return # cancelled / no selection
+
+    set -l key $sel[1] # key pressed (from --expect)
+    set -l line $sel[2] # the picked job line
+    set -l job (string split -f1 \t -- $line) # job number = first column
+
+    switch $key
+        case ctrl-b
+            bg %$job
+        case '*'
+            fg %$job
+    end
+end
